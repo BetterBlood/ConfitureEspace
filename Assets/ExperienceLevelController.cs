338eefdaebf53ece5586c0c01864b4693cf7138c
DazTruc <d.angeloz2106@gmail.com>
2025-03-15 18:10:24 +0100

    je commit parce que c'est bien
    UI d'xp presque finie, manque les panneaux de choix de stats

---
+++ Assets/ExperienceLevelController.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ExperienceLevelController : MonoBehaviour
+{
+
+    public static ExperienceLevelController instance;
+
+    private void Awake()
+    {
+      instance = this;
+    }
+    public int currentExperience;
+
+    public ExpPickup pickup;
+
+    public List<int> expLevels;
+    public int currentLevel = 1, levelCount = 50;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        while(expLevels.Count < levelCount)
+        {
+            expLevels.Add(Mathf.CeilToInt(expLevels[expLevels.Count - 1] * 1.1f));
+                
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GetExp(int amountToGet)
+    {
+        currentExperience += amountToGet;
+
+        // level up
+        if(currentExperience >= expLevels[currentLevel])
+        {
+            LevelUp();
+        }
+
+        UIController.instance.UpdateExperience(currentExperience, expLevels[currentLevel], currentLevel);
+    }
+
+    private void LevelUp()
+    {
+        currentExperience -= expLevels[currentLevel];
+
+        currentLevel++;
+        if(currentLevel >= expLevels.Count)
+        {
+            currentLevel = expLevels.Count - 1;
+        }
+    }
+
+}
