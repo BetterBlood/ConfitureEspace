5f8800213167a7b0b7167b61293bb98b238f2eb3
Better_Blood <better_blood@hotmail.com>
2025-03-16 04:34:34 +0100

    WIP on Peter

--- Assets/Scripts/LevelUpMenu.cs
+++ Assets/Scripts/LevelUpMenu.cs
@@ -5,21 +5,24 @@ public class LevelUpMenu : MonoBehaviour
 {
     public static LevelUpMenu instance;
 
-
+    public int[] choices = new int[3];
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    
     private void Awake()
     {
         instance = this;
     }
 
     public GameObject levelUpMenuUI;
-    //public Button Button1;
-    //public Button Button2;
-    //public Button Button3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        choices[0] = 0;
+        choices[1] = 1;
+        choices[2] = 2;
     }
 
     // Update is called once per frame
@@ -27,18 +30,29 @@ public class LevelUpMenu : MonoBehaviour
     {
 
     }
-
+    
     public void BonusChoiceMenu()
     {
         Time.timeScale = 0f;
         levelUpMenuUI.SetActive(true);
-        //ButtonChoice(); // ICI BRYAN ET JEREMIAH
-        //levelUpMenuUI.SetActive(false);
-        //Time.timeScale = 1f;
+        choices[0] = Random.Range(0, 6);
+        choices[1] = Random.Range(0, 6);
+        while (choices[1] == choices[0]) choices[1] = Random.Range(0, 6);
+        while (choices[2] == choices[0] || choices[2] == choices[1]) choices[2] = Random.Range(0, 6);
 
     }
 
-    public void ButtonChoice()
+    public void ButtonChoice1()
+    {
+        // ICI AUSSI BRYAN ET JEREMIAH
+    }
+
+    public void ButtonChoice2()
+    {
+        // ICI AUSSI BRYAN ET JEREMIAH
+    }
+
+    public void ButtonChoice3()
     {
         // ICI AUSSI BRYAN ET JEREMIAH
     }
